da526a930abdf02ef16981fb6289271c9df9952f
Daniel Ng <daniel.ng@midsummerenergy.co.uk>
2022-11-11 07:18:22 +0000

    filteredOrder

--- utils/svg.js
+++ utils/svg.js
@@ -9,10 +9,11 @@ const generate = (points, salesmenCapacities, order, showString = false, isLoop 
     salesmenCapacities.forEach((capacity, i) => {
         const rangeStart = salesmenCapacities.slice(0, i).reduce((a, b) => a + b, 0);
         const rangeEnd = salesmenCapacities.slice(0, i + 1).reduce((a, b) => a + b, 0);
-        const filteredPoints = [];
+        const filteredOrder = []
         for (let j = rangeStart; j < rangeEnd; j++) {
-            filteredPoints.push(points[order[j]]);
+            filteredOrder.push(order[j]);
         }
+        const filteredPoints = filteredOrder.map(o => points[o]);
         const color = colors[i % colors.length];
         filteredPoints.forEach((p) => {
             result += `<circle cx="${(p.x + 1) * width / 2}" cy="${(-p.y + 1) * height / 2}" r="5" stroke="black" stroke-width="1" fill="${color}" />`;
@@ -30,6 +31,8 @@ const generate = (points, salesmenCapacities, order, showString = false, isLoop 
             let textY = filteredPoints[0].y;
             let offsetX = -5;
             let offsetY = 7;
+
+
             let text = `n=${capacity}`;
             let textString = `<text x="${(textX + 1) * width / 2 + offsetX}" y="${(-textY + 1) * height / 2 - offsetY}" font-size="10" font-weight="700" fill="${color}">${text}</text>`;
             result += textString;
